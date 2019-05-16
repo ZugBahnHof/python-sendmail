680adc717fbb7243e348f9e72e2ae1d7ad32f060
Julian Leucker <leuckerj@gmail.com>
2019-05-16 12:52:49 +0200

    function to send mails

--- sendmail.py
+++ sendmail.py
@@ -1,7 +1,10 @@
 import functions
 import smtplib
 
-SERVER = ""
-PORT = ""
-USER = ""
-PASSWORD = ""
+def send(addr_to="", message):
+
+    SERVER, PORT, USER, PASSWORD = functions.parseCredentials()
+
+    connection = smtplib.SMTP(host=SERVER, port=PORT)
+    connection.sendmail(USER, addr_to, message)
+    connection.quit()
