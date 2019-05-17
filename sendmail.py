dba65ed5f901744fa7a16bfb8a5e7d467893e4bc
Julian Leucker <leuckerj@gmail.com>
2019-05-17 11:51:37 +0200

    correct function to send mails

--- sendmail.py
+++ sendmail.py
@@ -1,10 +1,13 @@
 import functions
 import smtplib
 
-def send(addr_to="", message):
+def send(addr_to="", message="Nachricht"):
 
     SERVER, PORT, USER, PASSWORD = functions.parseCredentials()
 
     connection = smtplib.SMTP(host=SERVER, port=PORT)
-    connection.sendmail(USER, addr_to, message)
+    connection.starttls()
+    connection.login(USER, PASSWORD)
+    connection.sendmail(from_addr=USER, to_addrs=addr_to, msg=message)
     connection.quit()
+    print("Nachricht wurde gesendet")
